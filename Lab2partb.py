1687aeb40cb18b946591526b4185f744c9eb687c
danielaboagye <aboagyed0820@students.bowiestate.edu>
2023-09-27 13:40:29 -0400

    Already submitted lab2part a and this one is lab 2partb

---
+++ Lab2partb.py
@@ -0,0 +1,43 @@
+number1= 10
+number2= 11
+number3= 12
+number4= 13
+
+addition =number1+number2+number3+number4
+
+subtraction=number4-number3-number2-number1
+
+multiplication = number1*number2*number3*number4
+
+division = (number4/number3)
+
+print(addition,subtraction,multiplication,division)
+
+number1= 11
+number2= 12
+number3= 13
+number4= 14
+
+addition =number1+number2+number3+number4
+
+subtraction=number4-number3-number2-number1
+
+multiplication = number1*number2*number3*number4
+
+division = (number4/number3)
+
+print(addition,subtraction,multiplication,division)
+
+number1= 10
+number2= 11
+
+
+addition =number1+number2
+
+subtraction=number2-number1
+
+multiplication = number1*number2
+
+division = (number2/number1)
+
+print(addition,subtraction,multiplication,division)
