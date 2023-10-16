c4fbc07f2867dc6bd66f62e348f39db676760cc2
danielaboagye <aboagyed0820@students.bowiestate.edu>
2023-10-16 14:09:17 -0400

    This is the LAB3 work

---
+++ LAB3.py
@@ -0,0 +1,37 @@
+import math
+
+number1=input('Enter number1')
+number2=input('Enter number2')
+number3=input('Enter number3')
+print(number1,number2,number3)
+
+number1=input('Enter number1')
+number2=input('Enter number2')
+number3=input('Enter number3')
+sum=number1+number2+number3
+print(sum)
+
+number1=input('Enter number1')
+number2=input('Enter number2')
+number3=input('Enter number3')
+addition= int(number1) + int(number2)+int(number3)
+print(addition)
+
+subtraction=int(number1)-int(number2)-int(number3)
+print(subtraction)
+
+division=int(number1)/int(number2)/int(number3)
+print(division)
+
+multiplication=int(number1)*int(number2)*int(number3)
+print(multiplication)
+print (math.acos(0.55,))
+print (math.acosh(7))
+print(math.asin(1))
+print(math.asinh(8))
+print(math.atan(0.42))
+print(math.atan2(9,5))
+print(math.atanh(0.52))
+print(math.ceil(1.4))
+print(math.comb(7,5))
+print(math.copysign(4,-1))
