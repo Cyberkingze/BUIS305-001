e93e563c59a697d6863c9f0ebae76aa754a0a5fb
danielaboagye <aboagyed0820@students.bowiestate.edu>
2023-11-01 15:38:20 -0400

    This is the LAB3 work

---
+++ Mid Term Question 3.py
@@ -0,0 +1,9 @@
+number1 =float(input('Enter number1'))
+number2 =float(input('Enter number2'))
+number3 =float(input('Enter number3'))
+number4 =float(input('Enter number4'))
+number5 =float(input('Enter number5'))
+sum=number1+number2+number3+number4+number5
+print(sum)
+average =(number1+number2+number3+number4+number5)/5
+print('average',average)
---
+++ Midterm Question 4.py
@@ -0,0 +1,14 @@
+# This type of look displays the texts on the same line because we did not add the end='' at the end of the charachter
+for character  in 'DANL-490':
+    print(character, end='')
+print()
+    # This one displays the texts on different lines with end=''
+
+
+# We can also use this to represent a loop
+for i in range (0,11):
+    print(i,end='')
+for i in range (1,11):
+    print(i,end='')
+for(i) in range  (1,11,2):
+    print (i,end='')
