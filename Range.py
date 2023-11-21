920ba5f49b4f3ec95ff9562e564b04f1b5f0314d
danielaboagye <aboagyed0820@students.bowiestate.edu>
2023-11-21 16:30:46 -0500

    This is the LAB5 work

---
+++ Range.py
@@ -0,0 +1,15 @@
+"examples to work in ranges"
+for i in range(4):
+    print("Test")
+    print("123")
+    print("i am here")
+"for number range"
+for i in range(1,101):
+    print(i)
+
+#statement of a given number"
+number1=5
+sum1=6
+for i in range(5):
+    addition1 =sum1+number1
+    print(sum1)
